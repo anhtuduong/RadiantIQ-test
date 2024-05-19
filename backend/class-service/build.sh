7556506eab3e12c783e71ccd4d9c373c2cbd43bc
Anh Tu Duong <anhtu.duong@studenti.unitn.it>
2024-05-19 23:36:00 +0200

    setup: setup class service

---
+++ backend/class-service/build.sh
@@ -0,0 +1,7 @@
+# ------------------------------------------------------------------------------
+# Description: This script builds the image of the
+# class-service.
+# ------------------------------------------------------------------------------
+
+# Build the image
+docker build -t radiantiq-class-service .
---
+++ backend/class-service/clean.sh
@@ -0,0 +1,7 @@
+# ------------------------------------------------------------------------------
+# Description: This script removes the image of the
+# class service.
+# ------------------------------------------------------------------------------
+
+# Remove the images
+docker rmi radiantiq-class-service
