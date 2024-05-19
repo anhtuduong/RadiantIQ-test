28a451fbc3050a167e6e547ba1f904afd602d3a4
Anh Tu Duong <anhtu.duong@studenti.unitn.it>
2024-05-20 00:00:29 +0200

    setup: setup payment service

---
+++ backend/payment-service/build.sh
@@ -0,0 +1,7 @@
+# ------------------------------------------------------------------------------
+# Description: This script builds the image of the
+# payment-service.
+# ------------------------------------------------------------------------------
+
+# Build the image
+docker build -t radiantiq-payment-service .
